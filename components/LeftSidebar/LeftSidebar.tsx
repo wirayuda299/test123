5afef15f492752cf3a9492ae7aa6a8943e0b813f
Hannah-Liao <hanna.liao@outlook.com>
2023-10-05 16:17:58 +0100

    refactor: fix responsiveness and semantic elements

--- components/LeftSidebar/LeftSidebar.tsx
+++ components/LeftSidebar/LeftSidebar.tsx
@@ -7,11 +7,10 @@ import Following from "./Following";
 
 const LeftSidebar = () => {
   return (
-    <div className="relative m-3 rounded-2xl bg-white text-center dark:bg-darkPrimary-3 md:w-[210px]">
+    <aside className="relative m-3 rounded-2xl bg-white text-center dark:bg-darkPrimary-3 md:w-[210px]">
+      <div className="h-[106px] w-full rounded-t-2xl bg-[url('../public/left-sidebar-bg.svg')]" />
       <div className="px-5 py-[30px]">
-        <div className="h-[106px] w-full rounded-t-2xl bg-[url('../public/left-sidebar-bg.svg')]" />
-
-        <div className="flexCenter absolute left-10 top-10 h-[130px] w-[130px] rounded-full border-4 border-white bg-secondary-yellow-30">
+        <div className="flexCenter absolute inset-x-0 top-10 mx-auto h-[130px] w-[130px] rounded-full border-4 border-white bg-secondary-yellow-30">
           <Image src="user-avatar.svg" alt="user-img" width={90} height={90} />
         </div>
 
@@ -35,42 +34,44 @@ const LeftSidebar = () => {
           support the other members of this community!
         </p>
 
-        <div className="flexCenter gap-[10px] py-5">
-          <Image
-            src="left-sidebar-web.svg"
-            alt="link-img"
-            width={14}
-            height={14}
-            className="dark:brightness-0 dark:invert"
-          />
-          <Link
-            href="www.uikit.to"
-            className="body-semibold text-darkSecondary-900 dark:text-white-700"
-          >
-            www.uikit.to
-          </Link>
-        </div>
-
-        <div className="flexCenter gap-[10px]">
-          {socialIcons.map((icon) => (
+        <div className="flexCenter mt-5 flex-row gap-5 md:flex-col">
+          <div className="flexCenter gap-[10px]">
             <Image
-              key={icon.label}
-              src={icon.path}
-              alt={icon.label}
-              width={16}
-              height={16}
-              className="dark:brightness-0 dark:invert"
+              src="left-sidebar-web.svg"
+              alt="link-img"
+              width={20}
+              height={20}
+              className="h-5 w-5 dark:brightness-0 dark:invert md:h-[14px] md:w-[14px]"
             />
-          ))}
+            <Link
+              href="www.uikit.to"
+              className="body-semibold text-darkSecondary-900 dark:text-white-700"
+            >
+              www.uikit.to
+            </Link>
+          </div>
+
+          <div className="flexCenter gap-[10px]">
+            {socialIcons.map((icon) => (
+              <Image
+                key={icon.label}
+                src={icon.path}
+                alt={icon.label}
+                width={16}
+                height={16}
+                className="dark:brightness-0 dark:invert"
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="my-[30px] border-b-[1px] border-white-800" />
+        <div className="mx-auto my-[30px] max-w-[170px] border-b-[1px] border-white-800 dark:border-darkSecondary-800" />
 
         <p className="body-semibold text-darkSecondary-800 dark:text-white-700">
           joined 2 years ago
         </p>
       </div>
-    </div>
+    </aside>
   );
 };
 
