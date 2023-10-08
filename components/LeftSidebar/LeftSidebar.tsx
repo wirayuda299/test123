4c801e0701ee9ef419d277267d7efbc415713e62
Hannah-Liao <hanna.liao@outlook.com>
2023-10-08 21:50:32 +0100

    feat:add profile page

--- components/LeftSidebar/LeftSidebar.tsx
+++ components/LeftSidebar/LeftSidebar.tsx
@@ -7,7 +7,7 @@ import Following from "./Following";
 
 const LeftSidebar = () => {
   return (
-    <aside className="relative m-3 rounded-2xl bg-white text-center dark:bg-darkPrimary-3 md:w-[210px]">
+    <aside className="relative rounded-2xl bg-white text-center dark:bg-darkPrimary-3 md:w-[210px]">
       <div className="h-[106px] w-full rounded-t-2xl bg-[url('../public/left-sidebar-bg.svg')]" />
       <div className="px-5 py-[30px]">
         <div className="flexCenter absolute inset-x-0 top-10 mx-auto h-[130px] w-[130px] rounded-full border-4 border-white bg-secondary-yellow-30">
--- components/Performance/Performance.tsx
+++ components/Performance/Performance.tsx
@@ -4,34 +4,45 @@ import { demoPerformance } from "@/constant";
 
 const Performance = () => {
   return (
-    <aside className="inline-flex flex-col items-start gap-[10px] rounded-2xl bg-white p-5 dark:bg-darkPrimary-3">
-      <h1 className="heading3 text-darkSecondary dark:text-white">
-        Performance
-      </h1>
-      <p className="display-regular text-darkSecondary-800 dark:text-white-700">
-        Showing data from the last 30 days
-      </p>
+    <aside>
+      <div className="h-[168px] w-[325px] rounded-2xl bg-orange-500">
+        Place holder
+      </div>
 
-      {demoPerformance.map((performance, i) => (
-        <div key={i} className="flex items-start gap-7">
-          <Image src={performance.img} alt="user-img" width={50} height={50} />
+      <div className="inline-flex flex-col items-start gap-[10px] rounded-2xl bg-white p-5 dark:bg-darkPrimary-3">
+        <h1 className="heading3 text-darkSecondary dark:text-white">
+          Performance
+        </h1>
+        <p className="display-regular text-darkSecondary-800 dark:text-white-700">
+          Showing data from the last 30 days
+        </p>
 
-          {performance.data.map((data) => (
-            <div key={data.title}>
-              <h2 className="display-regular text-darkSecondary-800 dark:text-white-700">
-                {data.title}
-              </h2>
-              <p className="body-semibold text-darkSecondary-900 dark:text-darkSecondary-800">
-                {data.content}
-              </p>
-            </div>
-          ))}
-        </div>
-      ))}
+        {demoPerformance.map((performance, i) => (
+          <div key={i} className="flex items-start gap-7">
+            <Image
+              src={performance.img}
+              alt="user-img"
+              width={50}
+              height={50}
+            />
 
-      <button className="body-semibold text-secondary-blue dark:text-secondary-blueBlack-80">
-        See More
-      </button>
+            {performance.data.map((data) => (
+              <div key={data.title}>
+                <h2 className="display-regular text-darkSecondary-800 dark:text-white-700">
+                  {data.title}
+                </h2>
+                <p className="body-semibold text-darkSecondary-900 dark:text-darkSecondary-800">
+                  {data.content}
+                </p>
+              </div>
+            ))}
+          </div>
+        ))}
+
+        <button className="body-semibold text-secondary-blue dark:text-secondary-blueBlack-80">
+          See More
+        </button>
+      </div>
     </aside>
   );
 };
