a8e2d551641a87352556e563fcdd50b833a5803c
Hayley <hayleyiscoding@gmail.com>
2023-10-09 20:54:20 +0800

    create constants

---
+++ constant/homeLeftSidebar.ts
@@ -0,0 +1,114 @@
+import { title } from 'process';
+
+export const homeLeftSideBar = [
+  {
+    id: 1,
+    icon:
+    title: "Newest and Recent",
+    text: "Find the latest update",
+  },
+  {
+    id: 2,
+    icon: ,
+    title: "Newest and Recent",
+    text: "Shots Featured Today by Createors",
+  },
+  {
+    id: 3
+    title: "Following",
+    icon: ,
+    text: "Explore from your favorite person"
+    number: 24
+},
+];
+
+export const popularTags = [
+    {
+        id: 1,
+        tag: javascript,
+        icon:,
+        noOfPosts: 82645,
+        text: "Posted by this tag"
+    },
+    {
+        id: 2,
+        tag: bitcoin,
+        icon:,
+        noOfPosts: 65523,
+        text: "Posted Trending"
+    },
+    {
+        id: 3,
+        tag: design,
+        icon:,
+        noOfPosts: 45637,
+        text: "Trending in Bangladesh"
+    },
+    {
+        id: 4,
+        tag: blogging,
+        icon:,
+        noOfPosts: 67645,
+        text: "Posted by this tag"
+    },
+    {
+        id: 5,
+        tag: tutorial,
+        icon:,
+        noOfPosts: 42625,
+        text: "Trending in Bangladesh"
+    },
+    {
+        id: 6,
+        tag: seo,
+        icon:,
+        noOfPosts: 55645,
+        text: "Posted by this tag"
+    }
+]
+
+
+export const pinnedGroups = [
+    {
+        id: 1,
+        tag: javascript,
+        icon:,
+        noOfPosts: 82645,
+        text: "Posted by this tag"
+    },
+    {
+        id: 2,
+        tag: bitcoin,
+        icon:,
+        noOfPosts: 65523,
+        text: "Posted Trending"
+    },
+    {
+        id: 3,
+        tag: design,
+        icon:,
+        noOfPosts: 45637,
+        text: "Trending in Bangladesh"
+    },
+    {
+        id: 4,
+        tag: blogging,
+        icon:,
+        noOfPosts: 67645,
+        text: "Posted by this tag"
+    },
+    {
+        id: 5,
+        tag: tutorial,
+        icon:,
+        noOfPosts: 42625,
+        text: "Trending in Bangladesh"
+    },
+    {
+        id: 6,
+        tag: seo,
+        icon:,
+        noOfPosts: 55645,
+        text: "Posted by this tag"
+    }
+]
