0ef8b20420d0df987fbc9b710da636431b53692a
surajarya130 <surajarya130@gmail.com>
2023-10-10 16:52:57 +0530

    resolved some merge and pusged new code

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,7 @@
-import HostMeetupCard from "./HostMeetupCard/HostMeetupCard";
-import SidePodcasts from "./SidePodcasts/SidePodcasts";
-import PodcastsCard from "./PodcastsCard/PodcastsCard";
-import MeetupsWrapper from "./MeetupsWrapper/MeetupsWrapper";
 export { default as Contents } from '@/components/Contents/Contents';
-
 export { default as BusinessInterest } from '@/components/BusinessInterest/BuisinessInterest';
-import Filter from "./Filter/Filter";
-
-export { HostMeetupCard, SidePodcasts, PodcastsCard, MeetupsWrapper, default as LeftSidebar, Filter } from "./LeftSidebar/LeftSidebar";
+export { default as HostMeetupCard } from '@/components/HostMeetupCard/HostMeetupCard';
+export { default as SidePodcasts } from '@/components/SidePodcasts/SidePodcasts';
+export { default as PodcastsCard } from '@/components/PodcastsCard/PodcastsCard';
+export { default as MeetupsWrapper } from '@/components/MeetupsWrapper/MeetupsWrapper';
+export { default as Filter } from '@/components/Filter/Filter';
