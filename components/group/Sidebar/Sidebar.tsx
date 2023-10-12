ac303912d78ab8edb6e347a625013b61a931973a
wirayuda <wirayuda233@gmail.com>
2023-10-12 22:48:14 +0800

    fix content overflow

--- components/group/Sidebar/Sidebar.tsx
+++ components/group/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
   return (
     <aside className='h-full min-w-[250px] rounded-lg bg-white dark:bg-darkPrimary-3 max-lg:min-w-full lg:sticky lg:top-0'>
       <Mobile />
-      <div className='hidden w-full overflow-y-hidden p-3 hover:overflow-y-auto lg:block lg:h-screen [&>*:not(:first-child)]:mt-5 '>
+      <div className='hidden w-full overflow-y-hidden p-3 hover:overflow-y-auto dark:bg-darkPrimary-3 lg:block lg:h-screen [&>*:not(:first-child)]:mt-5'>
         {sidebarContents.map((content) => (
           <SidebarItem key={content.label} content={content} />
         ))}
