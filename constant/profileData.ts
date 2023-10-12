ad7818b2047f4c5316e902410ee3204c2f07d409
Hannah-Liao <hanna.liao@outlook.com>
2023-10-12 22:00:27 +0100

    refactor

---
+++ constant/profileData.ts
@@ -0,0 +1,73 @@
+export const socialIcons = [
+  {
+    label: 'twitter',
+    path: 'twitter.svg',
+  },
+  {
+    label: 'facebook',
+    path: 'facebook.svg',
+  },
+  {
+    label: 'insta',
+    path: 'instagram.svg',
+  },
+];
+
+export const demoPerformance = [
+  {
+    img: 'performance1.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+  {
+    img: 'performance2.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+  {
+    img: 'performance3.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+  {
+    img: 'performance4.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+  {
+    img: 'performance5.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+  {
+    img: 'performance6.svg',
+    data: [
+      { title: 'Views', content: '651,324' },
+      { title: 'Like', content: '36,6545' },
+      { title: 'Comments', content: '56' },
+    ],
+  },
+];
+
+export const optionBar = [
+  'Posts',
+  'Meetups',
+  'Podcasts',
+  'Interviews',
+  'History',
+];
