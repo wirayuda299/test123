40c015c5a26e9d5ca14f71a664eeb8d7f8bf9ff7
Hannah-Liao <hanna.liao@outlook.com>
2023-10-13 13:27:40 +0100

    refactor: change folder structure

---
+++ components/ProfilePage/ProfileFeed.tsx
@@ -0,0 +1,27 @@
+import { optionBar } from '@/constant';
+const demoFeeds = [1, 2, 3, 4];
+
+const ProfileFeed = () => {
+  return (
+    <main className='flex flex-col gap-[30px]'>
+      <div className='inline-flex items-start justify-between gap-[10px] rounded-[20px] bg-white px-[30px] py-[19px] dark:bg-darkPrimary-3 md:max-w-[785px]'>
+        {optionBar.map((option) => (
+          <button
+            key={option}
+            className='heading3 rounded-3xl px-5 py-2 text-darkSecondary-800 focus:bg-secondary-red-80 focus:text-white dark:text-white-700'
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+
+      {demoFeeds.map((feed, i) => (
+        <div key={i} className='h-[196px] w-[785px] rounded-2xl bg-[orange]'>
+          Something here
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default ProfileFeed;
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
 export { default as Contents } from '@/components/Contents/Contents';
 export { default as BusinessInterest } from '@/components/BusinessInterest/BuisinessInterest';
-export { default as LeftSidebar } from './LeftSidebar/LeftSidebar';
-export { default as Performance } from './Performance/Performance';
-export { default as ProfileFeed } from './ProfileFeed/ProfileFeed';
+export { default as LeftSidebar } from './ProfilePage/LeftSidebar/LeftSidebar';
+export { default as Performance } from './ProfilePage/Performance';
+export { default as ProfileFeed } from './ProfilePage/ProfileFeed';
