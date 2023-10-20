680b9d73cb8f21448b6f4a5466830b50e31b43a8
wirayuda <wirayuda233@gmail.com>
2023-10-20 23:44:19 +0800

    add missing component

--- app/(site)/group/[id]/page.tsx
+++ app/(site)/group/[id]/page.tsx
@@ -6,34 +6,29 @@ import {
   GroupAdmins,
   GroupBanner,
   GroupRecentMedia,
+  HostMeetupCard,
   Pagination,
 } from '@/components/index';
+import PopularTags from '@/components/Home/PopularTags';
 
-// this will be deleted later when reusable createPost component merged
-const CreatePost = () => {
-  return (
-    <div className=' min-h-[200px] w-full rounded-2xl bg-secondary-red-90'>
-      Create post component
-    </div>
-  );
-};
-// this will be deleted later when reusable PopularTag component merged
-const PopularTag = () => {
-  return (
-    <div className='min-h-[100px] w-full rounded-2xl bg-secondary-red-90'>
-      PopularTag component
-    </div>
-  );
-};
-
+const cardBtns = [
+  {
+    name: 'Create Group',
+    link: '/',
+  },
+  {
+    name: 'Code of Conduct',
+    link: '/code-of-conduct',
+  },
+];
 export default function GroupDetail() {
   return (
     <div className='h-full min-h-full w-full bg-white-800 p-5 dark:bg-darkPrimary-2'>
       <div className='flex w-full justify-between gap-5 max-lg:flex-wrap'>
-        <aside className='top-0 hidden h-full max-h-screen w-full max-w-[210px] gap-5 lg:sticky lg:flex lg:flex-col'>
+        <aside className='top-0 hidden h-full max-h-screen w-full max-w-[210px] gap-5 overflow-y-auto lg:sticky lg:flex lg:flex-col'>
           <GroupAbout />
           <GroupAdmins />
-          <PopularTag />
+          <PopularTags />
         </aside>
         <section className=' w-full grow '>
           <GroupBanner />
@@ -58,11 +53,11 @@ export default function GroupDetail() {
           <div className='flex flex-wrap gap-5 lg:hidden'>
             <GroupAbout />
             <GroupAdmins />
-            <PopularTag />
+            <PopularTags />
           </div>
         </section>
         <aside className='flex h-full w-full max-w-[325px] flex-col gap-5 overflow-y-auto max-lg:max-w-full  lg:sticky lg:top-0'>
-          <CreatePost />
+          <HostMeetupCard cardBtns={cardBtns} />
           <GroupActiveMembers />
           <GroupRecentMedia />
         </aside>
