08a851ff199d14a1d7dc2f814828113c44946be0
Hannah-Liao <hanna.liao@outlook.com>
2023-10-20 16:51:02 +0100

    refactor: fix responsiveness and add pagination

--- components/ProfilePage/OptionBar.tsx
+++ components/ProfilePage/OptionBar.tsx
@@ -2,17 +2,15 @@ import { optionBar } from '@/constant';
 
 const OptionBar = () => {
   return (
-    <div className='overflow-x-scroll'>
-      <div className='inline-flex w-full items-start justify-between rounded-[20px] bg-white px-[15px] py-[10px] dark:bg-darkPrimary-3 md:px-[30px] md:py-[19px] lg:max-w-[785px]'>
-        {optionBar.map((option) => (
-          <button
-            key={option}
-            className='heading3 rounded-3xl px-5 py-2 text-darkSecondary-800 focus:bg-secondary-red-80 focus:text-white dark:text-white-700'
-          >
-            {option}
-          </button>
-        ))}
-      </div>
+    <div className='no-scrollbar inline-flex w-full items-start justify-between overflow-x-scroll rounded-[20px] bg-white px-[15px] py-[10px] dark:bg-darkPrimary-3 md:px-[30px] md:py-[19px]'>
+      {optionBar.map((option) => (
+        <button
+          key={option}
+          className='heading3 rounded-3xl px-5 py-2 text-darkSecondary-800 focus:bg-secondary-red-80 focus:text-white dark:text-white-700'
+        >
+          {option}
+        </button>
+      ))}
     </div>
   );
 };
--- components/ProfilePage/Performance.tsx
+++ components/ProfilePage/Performance.tsx
@@ -4,46 +4,35 @@ import { demoPerformance } from '@/constant';
 
 const Performance = () => {
   return (
-    <aside className='w-full lg:sticky lg:top-0 lg:max-w-[325px]'>
-      <div className='h-[168px] w-[325px] rounded-2xl bg-orange-500 '>
-        Place holder
-      </div>
+    <div className='flex flex-col items-start gap-[10px] rounded-2xl bg-white p-5 dark:bg-darkPrimary-3'>
+      <h1 className='heading3 text-darkSecondary dark:text-white'>
+        Performance
+      </h1>
+      <p className='display-regular text-darkSecondary-800 dark:text-white-700'>
+        Showing data from the last 30 days
+      </p>
 
-      <div className='flex flex-col items-start gap-[10px] rounded-2xl bg-white p-5 dark:bg-darkPrimary-3'>
-        <h1 className='heading3 text-darkSecondary dark:text-white'>
-          Performance
-        </h1>
-        <p className='display-regular text-darkSecondary-800 dark:text-white-700'>
-          Showing data from the last 30 days
-        </p>
+      {demoPerformance.map((performance, i) => (
+        <div key={i} className='flex w-full items-start justify-between'>
+          <Image src={performance.img} alt='user-img' width={50} height={50} />
 
-        {demoPerformance.map((performance, i) => (
-          <div key={i} className='flex w-full items-start justify-between'>
-            <Image
-              src={performance.img}
-              alt='user-img'
-              width={50}
-              height={50}
-            />
+          {performance.data.map((data) => (
+            <div key={data.title}>
+              <h2 className='display-regular text-darkSecondary-800 dark:text-white-700'>
+                {data.title}
+              </h2>
+              <p className='body-semibold text-darkSecondary-900 dark:text-darkSecondary-800'>
+                {data.content}
+              </p>
+            </div>
+          ))}
+        </div>
+      ))}
 
-            {performance.data.map((data) => (
-              <div key={data.title}>
-                <h2 className='display-regular text-darkSecondary-800 dark:text-white-700'>
-                  {data.title}
-                </h2>
-                <p className='body-semibold text-darkSecondary-900 dark:text-darkSecondary-800'>
-                  {data.content}
-                </p>
-              </div>
-            ))}
-          </div>
-        ))}
-
-        <button className='body-semibold text-secondary-blue dark:text-secondary-blueBlack-80'>
-          See More
-        </button>
-      </div>
-    </aside>
+      <button className='body-semibold text-secondary-blue dark:text-secondary-blueBlack-80'>
+        See More
+      </button>
+    </div>
   );
 };
 
