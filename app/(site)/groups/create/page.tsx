592ad297d4ac61ff961e30b324c44d55aa89f421
wirayuda <wirayuda233@gmail.com>
2023-10-25 11:17:35 +0800

    create group form UI

--- app/(site)/groups/create/page.tsx
+++ app/(site)/groups/create/page.tsx
@@ -1,3 +1,9 @@
+import { CreateGroupForm } from '@/components/index';
+
 export default function CreateGroup() {
-  return <div>Create Groups</div>;
+  return (
+    <div className='mt-28 flex w-full justify-center'>
+      <CreateGroupForm />
+    </div>
+  );
 }
