7a4007fc604aa925a5dda25bf86d973994d1e778
surajarya130 <surajarya130@gmail.com>
2023-10-31 13:01:36 +0530

    removed extra files

--- lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-// import { PrismaClient } from '@prisma/client';
-
-// declare global {
-//   let prisma: PrismaClient | undefined;
-// }
-
-// export const prisma = global.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
