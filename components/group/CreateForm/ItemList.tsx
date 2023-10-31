1924ea6f088e29809b568dca3c89b36a7fd72e4b
wirayuda <wirayuda233@gmail.com>
2023-10-31 20:55:23 +0800

    add trash icon

--- components/group/CreateForm/ItemList.tsx
+++ components/group/CreateForm/ItemList.tsx
@@ -1,6 +1,7 @@
 import type { Dispatch, SetStateAction } from 'react';
 
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 type ItemListProps = {
   items: string[];
@@ -14,17 +15,20 @@ export default function ItemList({ items, setItems }: ItemListProps) {
       {items.slice(0, 3).map((admin) => (
         <Button
           type='button'
-          className='relative w-20 bg-white-800 text-darkPrimary-2 hover:bg-white-700 dark:bg-darkPrimary-4 dark:text-white-800 dark:hover:bg-darkPrimary-3'
+          className='group relative w-20 bg-white-800 text-darkPrimary-2 hover:bg-white-700 dark:bg-darkPrimary-4 dark:text-white-800 dark:hover:bg-darkPrimary-3'
           key={admin}
         >
-          <span
-            className='absolute right-0 top-0 cursor-pointer'
+          <Image
+            src={'/assets/icons/trash.svg'}
+            width={20}
+            height={20}
+            alt='trash icon'
+            className='absolute -right-1 -top-1 hidden h-3 w-3 cursor-pointer group-hover:block dark:invert'
             onClick={() =>
               setItems((prev) => prev.filter((current) => current !== admin))
             }
-          >
-            x
-          </span>
+          />
+
           {admin}
         </Button>
       ))}
