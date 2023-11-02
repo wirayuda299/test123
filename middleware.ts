ef014602c8a6f2403603ce5fb8441e6b54bbcbb9
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-02 12:35:55 +0530

    fix: spelling

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   publicRoutes: ['/sign-up', '/sign-in', '/'],
-  ignoredRoutes: ['/api/webhook'],
+  ignoredRoutes: ['/api/webhooks'],
 });
 
 export const config = {
