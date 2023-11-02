01f0717754d8c40de8ff72705016813925022965
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-02 11:56:02 +0530

    fix: prisma connection

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,22 +1,17 @@
-/* eslint-disable no-unused-vars */
-// @ts-nocheck
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-declare global {
-  namespace nodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
