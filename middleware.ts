ea9005a9fee617cf74e138ad39a897130206ee86
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-02 12:33:27 +0530

    feat: clerk ignore webhook route

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   publicRoutes: ['/sign-up', '/sign-in', '/'],
+  ignoredRoutes: ['/api/webhook'],
 });
 
 export const config = {
