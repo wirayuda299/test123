d89568e749012ac898de820a091ec0cd4b445aa6
Hannah-Liao <hanna.liao@outlook.com>
2023-11-09 13:05:28 +0000

    refactor

--- components/forms/CreatePost.tsx
+++ components/forms/CreatePost.tsx
@@ -26,11 +26,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
 import { CreatePostSchema } from '@/lib/validations';
 import { createPostData, sidebarContents } from '@/constant';
 
