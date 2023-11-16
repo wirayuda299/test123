6f67efbfde470dd65444dafad31ee55265be3071
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-16 17:21:38 +0530

    fix: poststas bug

--- components/post/PostStats.tsx
+++ components/post/PostStats.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { postStats } from '@/constants/post';
-import Report from './Report';
 
 const PostStats = () => {
   return (
     <div className='flex w-full shrink-0 gap-2.5 rounded-2xl bg-white p-5 px-7 dark:bg-darkPrimary-3'>
-      <ul className='flex w-full shrink-0 flex-col gap-5'>
+      <ul className='flex shrink-0 flex-col gap-5'>
         {postStats.map((item) => (
           <li key={item.id}>
             <div className='flex items-center justify-start gap-3.5'>
@@ -39,8 +39,19 @@ const PostStats = () => {
         ))}
 
         {/* Report */}
-        <li className='w-full cursor-pointer'>
-          <Report user='Mansurel Haque' />
+        <li className='cursor-pointer'>
+          <Link href='' className='flex items-center justify-start gap-3.5'>
+            <div className='flex aspect-square items-center justify-center rounded-[6px] bg-white-800 p-1 dark:bg-darkPrimary-3 dark:md:bg-darkPrimary-4'>
+              <Image
+                src='/assets/posts/report.svg'
+                alt='Report icon'
+                width={20}
+                height={20}
+                className='object-contain dark:brightness-200'
+              />
+            </div>
+            <p className='display-regular text-darkSecondary-800'>Report</p>
+          </Link>
         </li>
       </ul>
     </div>
